4550ad67bb8ea34b02352a046f5dc7cbd87349ad
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-07-25 22:15:39 +0100

    setup and build header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+import image from '../../images/img.PNG';
+
+const Header = () => {
+  return (
+    <header className={styles.root}>
+      <div className={styles.logo}>Glow</div>
+      <div className={styles.routes_container}>
+        <div className={styles.routes}>
+          {' '}
+          <p className={styles.route_link}>Product</p>
+          <p className={styles.route_link}>About</p>
+          <p className={styles.route_link}>Blog</p>
+          <p className={styles.route_link}>Reviews</p>
+        </div>
+        <div>
+          <p className={styles.route_link}>
+            <img src={image} alt="" width="50px" />
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../../components/header/Header';
+
+const Home = () => {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
+
+export default Home;
