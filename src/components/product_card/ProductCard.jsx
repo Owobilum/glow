b80108941dadbdc407ecd81ad9c48d910942af2b
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-07-26 21:03:41 +0100

    implement product card hover state

--- src/components/product_card/ProductCard.jsx
+++ src/components/product_card/ProductCard.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 import styles from './ProductCard.module.css';
+import likeIcon from '../../images/like.PNG';
+import bagIcon from '../../images/bag.PNG';
 
 const ProductCard = ({ product: { name, img } }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div className={styles.container}>
-      <p className={styles.img_box}>
+      <div
+        className={styles.img_box}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered && (
+          <div className={styles.actions}>
+            <button className={styles.buy_btn}>
+              {' '}
+              <img src={bagIcon} alt="bag" />
+            </button>
+            <button className={styles.like_btn}>
+              <img src={likeIcon} alt="like" />{' '}
+            </button>
+          </div>
+        )}
         <img src={img} alt="" className={styles.img} />
-      </p>
+      </div>
       <p className={styles.name}>{name}</p>
       <p className={styles.price}>$190.99</p>
     </div>
